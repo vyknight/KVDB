0e282d9dd3d58655f87d7694af0c24965a5f44b3
Zekun Liu <knightadark101@gmail.com>
2025-12-07 04:45:57 -0500

    Revert "Added tests for step 2 classes and debugged"
    This reverts commit 482e8f72c1da0ba019ac80f3714a67751c99dff3.

--- Page.h
+++ Page.h
@@ -1,3 +1,7 @@
+//
+// Created by K on 2025-12-06.
+//
+
 #ifndef KVDB_PAGE_H
 #define KVDB_PAGE_H
 
@@ -5,8 +9,6 @@
 #include <cstdint>
 #include <ctime>
 #include <memory>
-#include <cstring>
-#include <type_traits>  // For std::aligned_storage
 
 class Page
 {
@@ -14,7 +16,7 @@ public:
     static constexpr size_t PAGE_SIZE = 4096;
 
     Page();
-    ~Page() = default;
+    ~Page();
 
     // no copy
     Page(const Page&) = delete;
@@ -26,17 +28,16 @@ public:
 
     // Getters
     const PageId& get_id() const { return id_; }
-    char* get_data() { return data_ptr(); }
-    const char* get_data() const { return data_ptr(); }
+    char* get_data() { return data_.get(); }
+    const char* get_data() const { return data_.get(); }
     bool is_dirty() const { return dirty_; }
     bool is_pinned() const { return pin_count_ > 0; }
     uint32_t get_pin_count() const { return pin_count_; }
     time_t get_last_access() const { return last_access_; }
-    time_t get_load_time() const { return load_time_; }
 
     // Setters
     void set_id(const PageId& id) { id_ = id; }
-    void set_dirty(const bool dirty) { dirty_ = dirty; }
+    void set_dirty(bool dirty) { dirty_ = dirty; }
     void mark_dirty() { dirty_ = true; }
     void clear_dirty() { dirty_ = false; }
 
@@ -59,15 +60,10 @@ public:
 
 private:
     void update_access_time();
-
-    // Helper to get pointer to aligned storage
-    char* data_ptr() { return reinterpret_cast<char*>(&storage_); }
-    const char* data_ptr() const { return reinterpret_cast<const char*>(&storage_); }
-
-    // Aligned storage for page data (aligned to 4096 bytes)
-    std::aligned_storage_t<PAGE_SIZE, 4096> storage_;
+    void allocate_buffer();
 
     PageId id_;
+    std::unique_ptr<char[]> data_;
     bool dirty_ = false;
     uint32_t pin_count_ = 0;
     time_t last_access_ = 0;
